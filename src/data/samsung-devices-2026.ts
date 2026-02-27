fa1b367ba7e9cfd55be5e3ee827add03fc069e90
kulcsarrudolf <kulcsarrudolf@gmail.com>
2026-02-27 19:52:56 +0200

    feat: added s26+ models

--- src/data/samsung-devices-2026.ts
+++ src/data/samsung-devices-2026.ts
@@ -9,4 +9,57 @@ export const samsungDevices2026: Device[] = [
       "SM-A076",
     ],
   },
+  {
+    "name": "Galaxy F70e",
+    "releaseDate": "02-17-2026",
+    "models": [
+      "SM-E076B",
+      "SM-E076B/DS"
+    ]
+  },
+  {
+    "name": "Galaxy S26",
+    "releaseDate": "03-06-2026",
+    "models": [
+      "SM-S942B",
+      "SM-S942B/DS",
+      "SM-S942U",
+      "SM-S942U1",
+      "SM-S942W",
+      "SM-S942N",
+      "SM-S9420",
+      "SM-S942E",
+      "SM-S942E/DS"
+    ]
+  },
+  {
+    "name": "Galaxy S26+",
+    "releaseDate": "03-06-2026",
+    "models": [
+      "SM-S947B",
+      "SM-S947B/DS",
+      "SM-S947U",
+      "SM-S947U1",
+      "SM-S947W",
+      "SM-S947N",
+      "SM-S9470",
+      "SM-S947E",
+      "SM-S947E/DS"
+    ]
+  },
+  {
+    "name": "Galaxy S26 Ultra",
+    "releaseDate": "03-06-2026",
+    "models": [
+      "SM-S948B",
+      "SM-S948B/DS",
+      "SM-S948U",
+      "SM-S948U1",
+      "SM-S948W",
+      "SM-S948N",
+      "SM-S9480",
+      "SM-S948E",
+      "SM-S948E/DS"
+    ]
+  }
 ];
