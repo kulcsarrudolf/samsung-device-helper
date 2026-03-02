aaa4f6a9ccc896cf553e513f7d770c5d8dcd8d84
kulcsarrudolf <kulcsarrudolf@gmail.com>
2026-03-02 21:18:18 +0200

    feat: test

--- src/data/samsung-devices-2026.ts
+++ src/data/samsung-devices-2026.ts
@@ -5,22 +5,17 @@ export const samsungDevices2026: Device[] = [
     name: "Galaxy A07",
     releaseDate: "01-13-2026",
     type: "phone",
+    models: ["SM-A076"],
+  },
+  {
+    name: "Galaxy F70e",
+    releaseDate: "02-17-2026",
+    models: ["SM-E076B", "SM-E076B/DS"],
+  },
+  {
+    name: "Galaxy S26",
+    releaseDate: "03-06-2026",
     models: [
-      "SM-A076",
-    ],
-  },
-  {
-    "name": "Galaxy F70e",
-    "releaseDate": "02-17-2026",
-    "models": [
-      "SM-E076B",
-      "SM-E076B/DS"
-    ]
-  },
-  {
-    "name": "Galaxy S26",
-    "releaseDate": "03-06-2026",
-    "models": [
       "SM-S942B",
       "SM-S942B/DS",
       "SM-S942U",
@@ -29,13 +24,13 @@ export const samsungDevices2026: Device[] = [
       "SM-S942N",
       "SM-S9420",
       "SM-S942E",
-      "SM-S942E/DS"
-    ]
+      "SM-S942E/DS",
+    ],
   },
   {
-    "name": "Galaxy S26+",
-    "releaseDate": "03-06-2026",
-    "models": [
+    name: "Galaxy S26+",
+    releaseDate: "03-06-2026",
+    models: [
       "SM-S947B",
       "SM-S947B/DS",
       "SM-S947U",
@@ -44,22 +39,7 @@ export const samsungDevices2026: Device[] = [
       "SM-S947N",
       "SM-S9470",
       "SM-S947E",
-      "SM-S947E/DS"
-    ]
+      "SM-S947E/DS",
+    ],
   },
-  {
-    "name": "Galaxy S26 Ultra",
-    "releaseDate": "03-06-2026",
-    "models": [
-      "SM-S948B",
-      "SM-S948B/DS",
-      "SM-S948U",
-      "SM-S948U1",
-      "SM-S948W",
-      "SM-S948N",
-      "SM-S9480",
-      "SM-S948E",
-      "SM-S948E/DS"
-    ]
-  }
 ];
